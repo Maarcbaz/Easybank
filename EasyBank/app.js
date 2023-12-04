45060c963c8c9edb9ae08c9b315b9105cc8b2596
Maarcbaz <138406453+Maarcbaz@users.noreply.github.com>
2023-12-04 01:03:46 +0100

    initial commit

---
+++ EasyBank/app.js
@@ -0,0 +1,10 @@
+function openNav() {
+  document.getElementById("myNav").style.width = "50%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
+const height = document.getElementById('topnav').style.height = '50px';
